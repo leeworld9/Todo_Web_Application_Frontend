9210ca45135b68ba7b02e80d410a7566c5bbb32e
dohelee <leeworld9@gmail.com>
2022-04-18 10:14:01 +0900

    ADD : todo CRUD add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Todo from './Todo';
+import { Paper, List, Container } from "@material-ui/core";
+import AddTodo from './AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items : [
+                {id : "0", title : "Hello Wrorld 1", done : true},
+                {id : "1", title : "Hello Wrorld 2", done : false},
+            ],
+        };
+    }
+
+    add = (item) => {
+        const thisItems = this.state.items;
+        item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+        item.done = false; // done 초기화
+        thisItems.push(item); // 리스트에 아이템 추가
+        this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야 됨
+        console.log("items : ", this.state.items);
+    }
+
+    delete = (item) => {
+        const thisItems = this.state.items;
+        console.log("Before Update Items : ", this.state.items)
+        const newItems = thisItems.filter(e => e.id !== item.id);
+        this.setState({ items: newItems }, () => {
+            // 디버깅 콜백
+            console.log("Update Items : ", this.state.items)
+        });
+    }
+
+    render() {
+        var todoItems = this.state.items.length > 0 && (
+            <Paper style={{ margin: 16 }}>
+                <List>
+                    {this.state.items.map((item, idx) => ( 
+                    <Todo item={item} key={item.id} delete={this.delete} />
+                    ))} 
+                </List>
+            </Paper> 
+        );
+            
+        return (
+            <div className="App">
+                <Container maxWidth="md">
+                    <AddTodo add={this.add}/>
+                    <div className="TodoList">{todoItems}</div>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render( // ReactDOM이 내부의 컴포넌트들을 'root' 엘리멘트에 render함 
+    <React.StrictMode>
+        <App /> 
+    </React.StrictMode>, 
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
