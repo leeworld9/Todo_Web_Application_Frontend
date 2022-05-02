0649cefc138a33c5a9e00f53743846b294edf684
dohelee <leeworld9@gmail.com>
2022-05-03 02:27:35 +0900

    update: Add front server address

--- src/app-config.js
+++ src/app-config.js
@@ -2,8 +2,10 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "localhost") { 
+if (hostname === "localhost") {
     backendHost = "http://localhost:8080";
+} else {
+    backendHost = "http://dohelee.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
